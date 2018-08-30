62f923db3b96207ddb0db6a99e6f957598cc7e66
Philip <fmesy@outlook.com>
2018-08-30 17:26:35 -0300

    Search Video

--- src/index.js
+++ src/index.js
@@ -13,10 +13,20 @@ const API_KEY = 'AIzaSyAB_5OA7L5mgEyTWKbLYqnSK6UTnNdcc28';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
       // this.setState({videos: videos});
     });
   }
@@ -24,9 +34,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} 
+        />
       </div>
     );
   }
